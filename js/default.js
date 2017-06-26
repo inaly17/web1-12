688c94ad98c79eec44422f6d552b252ee92599d0
inaly17 <inaly0102@softbank.ne.jp>
2017-06-26 17:09:26 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,8 @@
 var li=document.createElement('li');
 li.textContetnt='アイテム';
 document.getElementById('list').appendChild(li);
+
+docmuent.getElementById('form').onsubmit=function()
+{
+window.alert('押されました！');
+};
