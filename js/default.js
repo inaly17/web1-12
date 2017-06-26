30dd3fa9b6901b80678edcbe5e25431702fcd75c
inaly17 <inaly0102@softbank.ne.jp>
2017-06-26 18:18:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -7,7 +7,7 @@ document.getElementById('form').onsubmit=function()
 window.alert(document.getElementById('form').word.value);
 };
 
-for(var i=1;i<word;i++)
+for(var i=1;i<word.value;i++)
 {
   var li=document.createElement('li');
   if(i%3==0)
