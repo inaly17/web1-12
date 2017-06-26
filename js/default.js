aef2c8be55ae1301d7d3957e504db936b6107b8b
inaly17 <inaly0102@softbank.ne.jp>
2017-06-26 17:47:42 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,3 +6,28 @@ docmuent.getElementById('form').onsubmit=function()
 {
 window.alert(document.getElementById('form').word.value);
 };
+
+for(var i=1;i<word;i++)
+{
+  var li=document.createElement('li');
+  if(i%3==0)
+  {
+    message='Fizz';
+  }
+  else if(i%5==0)
+  {
+    message='Buzz';
+  }
+  else if(i%3==0&&i%5==0)
+  {
+    message='Fizz Buzz';
+  }
+  else
+  {
+    message=i;
+  }
+  
+  document.getElementById('choice').textContent =message;
+    
+  
+
